5adc62b7478ae50ec85761c9d2894a9fe0d9b8d5
Piotr Zaniewski <piotr@upbound.io>
2023-04-24 12:58:03 +0200

    Setting script flags

--- scripts/uploader.sh
+++ scripts/uploader.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# The set -e option instructs bash to immediately exit if any command has a non-zero exit status
+# The set -o pipefaile if any command in a pipeline fails, that return code will be used as the return code of the whole pipeline
+# https://bit.ly/37nFgin
+set -eo pipefail
 
 # Set variables
 NAMESPACE="prometheus"
